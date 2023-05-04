7244117775110513e6346b3796598f82ba583183
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-05-04 19:46:42 +0200

    - Added `decodeTo!(T)(byte[])`

---
+++ source/davinci/tools/package.d
@@ -0,0 +1,9 @@
+module davinci.tools;
+
+// TODO: Add a way to enforce that T must be derived from or equal to BaseMessage
+public T decodeTo(T)(byte[] data)
+{
+    // Currently we are using message pack
+    import msgpack;
+    return unpack!(T)(data);
+}
